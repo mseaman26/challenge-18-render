7f570e56f95abbf5ad57f07987dfc556cc938def
michael seaman <mseaman26@gmail.com>
2025-03-03 15:33:39 -0800

    better code from kyle

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,19 @@
-import express from 'express';
-// Import the ApolloServer class
-import {
-  ApolloServer,
-} from '@apollo/server';
-import {
-  expressMiddleware
-} from '@apollo/server/express4';
-import { authenticateToken } from './services/auth-service.js';
-// Import the two parts of a GraphQL schema
-import { typeDefs, resolvers } from './schemas/index.js';
-import db from './config/connection.js';
 
+import express from "express";
+import type { Request, Response } from "express";
+// Import the ApolloServer class
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { authenticateToken } from "./services/auth-service.js";
+// Import the two parts of a GraphQL schema
+import { typeDefs, resolvers } from "./schemas/index.js";
+import path from "node:path";
+import db from "./config/connection.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -20,8 +23,6 @@ const server = new ApolloServer({
 
 const app = express();
 
-//not included in develop
-
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
@@ -30,19 +31,25 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }
-  ));
+  app.use(
+    "/graphql",
+    expressMiddleware(server as any, {
+      context: authenticateToken as any,
+    })
+  );
 
-app.use(express.static('../client/dist'));
+  // if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+    app.get("*", (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+    });
+  // }
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
-
 };
 
 // Call the async function to start the server
