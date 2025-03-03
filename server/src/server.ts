6a21b5a3b73a7688176e37629e76bd6b1274b2d4
michael seaman <mseaman26@gmail.com>
2025-03-02 18:22:16 -0800

    I don't know

--- server/src/server.ts
+++ server/src/server.ts
@@ -43,9 +43,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
+      //altered this path
       res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
     });
   }
